294d85fed0de27a7dfc8a0e9218bd86eb7620c0b
kemoik <kemoicaleb17@gmail.com>
2023-05-09 01:20:33 +0300

    created react app and components

---
+++ greengold/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '58torqt4',
+    dataset: 'production'
+  }
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Layout } from 'components';
+import '../styles/globals.css';
+// import { StateContext } from 'context/StateContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+   
+  )
 }
+
+export default MyApp
